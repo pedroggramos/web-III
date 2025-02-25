6505e77246897ffeafa39981b18e035ff50c0ac4
Pedro Ramos <pedrogabrielguimaraesramos178@gmail.com>
2025-02-25 21:49:54 +0000

    up

---
+++ studies/Tupla e Lista/aula4.py
@@ -0,0 +1,29 @@
+#tuplas() - conjunto de dados,é imutavel, é representado pelo ()
+
+# a = (1,2,3,4,5,'Thereza',True)
+# print(a)
+# print(a[4])
+# print(a[-3])
+# print(a[1:4])
+# print(a[3:])
+# print(a[:3])
+
+# b = (1,2,3,4,5,'Gabi', 'Pedro', True)
+
+# print(b[5:7])
+
+# concatenação de tupla - + -------------------------------------
+
+# b = (1,2,3,4,5)
+# c = (6,7,8,9,10)
+# d = b + c
+# print(d)
+# print(b + c)
+
+
+#len() - mostra a quantidade de elementos de uma tupla ou lista -----
+
+# nome = ('Thereza','Julia','Paulo','Marcelo')
+# t = len(nome)
+# print(f'A quantidade de elementos da tupla é {t}')
+# print(f'A quantidade de elementos da tupla é {len(nome)}')
