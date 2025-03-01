9778ad55a740ce244ad1b24c92d19f5c7b6d6434
Pedro Ramos <pedrogabrielguimaraesramos178@gmail.com>
2025-03-01 15:32:07 -0300

    up

--- studies/tde's/tde3.py
+++ studies/tde's/tde3.py
@@ -50,4 +50,53 @@
 
 # print([num for num in lista if num > 10])
 
+# 6 -------------------------------
+
+# alunos = ['Pedro', 'Gabi', 'Valentina']
+# matriz = [[1,2,3,4],[4,5,6,7],[7,8,9,10]]
+
+# maior_media = 0
+# alunoMaior = ''
+
+# print(matriz[1][2])
+
+# for i in range(0, len(matriz)):
+#     media = sum(matriz[i]) / len(matriz[i])
+
+#     if media > maior_media:
+#         maior_media = media 
+#         alunoMaior = alunos[i]
+
+# print(f"O aluno com a maior média é: {alunoMaior} com média {maior_media:.2f}")
+
+# 7 -------------------------------
+
+# lista = [0] * 4
+
+# for i in range(len(lista)):
+#     lista[i] = float(input(f"Digite o numero: "))
+
+# print(f'O numero 9 apareceu {lista.count(9)} vezes')
+# print(f'O primeiro valor 3 está na posição {lista.index(3)}')
+# pares = [num for num in lista if num % 2 == 0]
+# print(f'Os numeros pares foram: {pares}')
+
+# 8 -------------------------------
+
+# import random
+
+# lancamentos = []
+
+# for _ in range(50):
+#     resultado = random.randint(1,6)
+#     lancamentos.append(resultado)
+
+# ocorrenciaSeis = lancamentos.count(6)
+# percentualSeis = (ocorrenciaSeis / 50) * 100
+
+# print(f'A face 6 apareceu {ocorrenciaSeis} vezes')
+# print(f'O percentual de ocorrencia da face 6 é de {percentualSeis:.2f}%')
+
+
+
 
