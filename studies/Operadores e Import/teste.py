8e981ca9b371b4166934ab816c1942afe641024e
Pedro Ramos <pedrogabrielguimaraesramos178@gmail.com>
2025-03-10 20:12:40 -0300

    up

---
+++ studies/Operadores e Import/teste.py
@@ -0,0 +1,27 @@
+import math
+
+
+# n1 = int(input("Digite um numero: "))
+# n2 = int(input("Digite um numero: "))
+
+# print(f"O resultado é {math.pow(n1,n2)}")
+# print(f"O resultado é {n1 ** n2}")
+
+
+# n3 = float(input("Digite um valor: "))
+
+# s = math.sqrt(n3)
+
+# print(f'A raiz quadrada {n3} = {round(s,2)}')
+# print(f'A raiz quadrada {s} = {math.ceil(s)}')
+# print(f'A raiz quadrada {s} = {math.floor(s)}')
+# print(f"A parte inteira do numero {n3} é {math.trunc(n3)}")
+
+import random
+
+r = random.random()
+
+print(f"{r:.1f}")
+
+
+
