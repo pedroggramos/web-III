45e34f89352ac32615e8e4d9e9c8ed6851f5c1f6
Pedro Ramos <pedrogabrielguimaraesramos178@gmail.com>
2025-07-30 23:24:29 -0300

    up

---
+++ htmlandcss/devClub/js/script.js
@@ -0,0 +1,38 @@
+// 
+//  JavaScript NÃO é Java
+//  Váriaveis(Qualquer linguagem de programação)
+// - Um pedacinho da memória do computador que vc separa pra guardar oq quiser
+// 
+// console.log() - ferramenta do JS que, que tudo q colocar dentro,
+//                  ele mostra no console
+// 
+//  Função só executa qnd é chamada
+// 
+//  Algoritimo(receita de bolo)
+//  Lógica de programação
+// 
+// 
+// 
+// 
+
+// let numero = 234543
+// let nome = 'Rodolfo'
+// let sobrenome = "mori"
+
+// console.log(numero);
+// console.log(nome);
+// console.log(sobrenome);
+
+// function digaOla(){
+//     console.log("Olá")
+// }
+
+// digaOla()
+
+// --------------------------
+
+let imagem = document.querySelector("#copao")
+
+function trocaImagem(endereco){
+    imagem.src = endereco
+}
